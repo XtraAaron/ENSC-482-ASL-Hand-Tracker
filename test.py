e89d2e0b664fc8fc755e71fdbc590734831bf0e1
XtraBunger <aaronyee2005@gmail.com>
2026-08-06 15:10:34 -0700

    Fixed code and added comments. Working basic camera code

--- test.py
+++ test.py
@@ -1,23 +1,27 @@
-import cv2
+import cv2 # Import OpenCV library
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW) # Creates a VideoCapture object
+# An object that knows how to communicate with the webcam
 
 if not cap.isOpened():
     print("Could not open camera.")
-    exit()
+    exit() # Ensures that the webcame is opened correctly
 
 print("Camera opened. Press 'q' to quit.")
 
 while True:
-    ret, frame = cap.read()
+    ret, frame = cap.read() # Takes a picture, if sucess ret is a boolian so true of false
+    # Frame is the image that has been taken
     if not ret:
         print("Failed to grab frame.")
         break
+    # Error handling for failed frame data
 
-    cv2.imshow("Camera Test", frame)
+    cv2.imshow("Camera Test", frame) # Display the frame in a seperate window
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'): # Wait refreshes the window, waits 1 ms and sees if a key was pressed
         break
+    # If q is pressed, break
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release() # Frees up webcam usage
+cv2.destroyAllWindows() # Closes opencv window
